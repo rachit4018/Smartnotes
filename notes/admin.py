ba1192888a49b46bec245a75de385c4af2748283
Rachit Pandya <a5558@MacBook-Air-3.local>
2023-11-07 17:05:41 -0500

    showing the table on admin site

--- notes/admin.py
+++ notes/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+
+# to register the model and class in admin site
+admin.site.register(models.Note, NoteAdmin)
--- notes/models.py
+++ notes/models.py
@@ -2,8 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class Notes(models.Model):
+class Note(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
-    
